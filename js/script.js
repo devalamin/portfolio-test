28096a3e5b740edd935390c9849b36d8c86db43d
devalamin <alamin.hasibmd2017@gmail.com>
2022-11-14 16:55:02 +0600

    portfolio

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// service slider js start
+
+
+$('.service-slider').slick({
+    dots: false,
+    arrows:true,
+    autoplay:true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode:true,
+    centerPadding:'0',
+    prevArrow:'<i class="fas fa-long-arrow-alt-left prev"></i>',
+    nextArrow:'<i class="fas fa-long-arrow-alt-right next"></i>',
+   
+  });
+  // service slider js end
+
+
+
+  // portfolio part start
+  var mixer = mixitup('.portflo');
+
+
+  // portfolio part end
+
+
+  // testimonial slider js start
+
+
+$('.test-slider').slick({
+  dots: false,
+  arrows:true,
+  autoplay:true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  prevArrow:'<i class="fas fa-long-arrow-alt-left prev"></i>',
+  nextArrow:'<i class="fas fa-long-arrow-alt-right next"></i>',
+
+  
+ 
+});
+// testimonial slider js end
